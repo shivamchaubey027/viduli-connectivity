d7f9dade581b8e69e39c21069739afbd111e68e1
shivamchaubey027 <shivamchaubey027@gmail.com>
2025-10-27 00:14:39 +0530

    initial

---
+++ database/database.go
@@ -0,0 +1,43 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// Item is our database model
+type Item struct {
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+var DB *gorm.DB
+
+// ConnectDB connects to the database using environment variables
+func ConnectDB() {
+	var err error
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("SSL_MODE"),
+	)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	fmt.Println("Database connected")
+
+	// Migrate the schema
+	DB.AutoMigrate(&Item{})
+}
