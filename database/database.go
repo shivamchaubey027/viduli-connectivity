2fc115288cbcaa8c342500e202e22c423c828704
shivamchaubey027 <shivamchaubey027@gmail.com>
2025-10-31 22:03:35 +0530

    Changes in dsn format

--- database/database.go
+++ database/database.go
@@ -20,14 +20,7 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	var err error
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("NEW_POSTGRES_DATABASE_HOST"),
-		os.Getenv("NEW_POSTGRES_DATABASE_USER"),
-		os.Getenv("NEW_POSTGRES_DATABASE_PASSWORD"),
-		os.Getenv("NEW_POSTGRES_DATABASE_DATA"),
-		os.Getenv("NEW_POSTGRES_DATABASE_PORT"),
-	)
-
+	dsn := os.Getenv("NEW_POSTGRES_DATABASE_URL")
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
