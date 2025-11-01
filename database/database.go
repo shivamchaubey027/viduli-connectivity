50c653eca14e99b6993e3f44e69307b6c33872e7
shivamchaubey027 <shivamchaubey027@gmail.com>
2025-11-01 23:00:23 +0530

    simplifying

--- database/database.go
+++ database/database.go
@@ -1,10 +1,8 @@
 package database
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"strings"
 	"time"
 
 	"gorm.io/driver/postgres"
@@ -22,45 +20,19 @@ type Item struct {
 	Description string
 }
 
+var db *gorm.DB
+
 func ConnectDB() {
-	var dsn string
-
-	dsn = os.Getenv("DATABASE_URL")
-
-	if dsn == "" {
-		host := os.Getenv("DB_HOST")
-		user := os.Getenv("DB_USER")
-		password := os.Getenv("DB_PASSWORD")
-		dbname := os.Getenv("DB_NAME")
-		port := os.Getenv("DB_PORT")
-		sslmode := os.Getenv("SSL_MODE")
-
-		if sslmode == "" {
-			sslmode = "disable"
-		}
-		if port == "" {
-			port = "5432"
-		}
-
-		dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
-			host, user, password, dbname, port, sslmode)
-	}
-
+	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
 		dsn = "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
 	}
-
-	log.Printf("Connecting to database with DSN: %s", maskPassword(dsn))
-
 	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
 
-	DB.AutoMigrate(&Item{})
-}
-
-func maskPassword(dsn string) string {
-	return strings.Replace(dsn, "password=", "password=***", 1)
+	// Migrate the schema
+	db.AutoMigrate(&Item{})
 }
