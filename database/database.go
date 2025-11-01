e208f03b598e3330e4dea96bc10c1af21efcf5a7
shivamchaubey027 <shivamchaubey027@gmail.com>
2025-11-01 23:20:25 +0530

    db

--- database/database.go
+++ database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -23,8 +24,47 @@ type Item struct {
 func ConnectDB() {
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
-		dsn = "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
+		// Build DSN from individual environment variables if DATABASE_URL isn't set
+		host := os.Getenv("DB_HOST")
+		port := os.Getenv("DB_PORT")
+		user := os.Getenv("DB_USER")
+		password := os.Getenv("DB_PASSWORD")
+		dbname := os.Getenv("DB_NAME")
+		sslmode := os.Getenv("SSL_MODE")
+
+		if host == "" {
+			host = "localhost"
+		}
+		if port == "" {
+			port = "5432"
+		}
+		if user == "" {
+			user = "postgres"
+		}
+		if password == "" {
+			password = "postgres"
+		}
+		if dbname == "" {
+			dbname = "postgres"
+		}
+		if sslmode == "" {
+			// default to disable for local development; change to "require" in production if needed
+			sslmode = "disable"
+		}
+
+		dsn = fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+			host, user, password, dbname, port, sslmode,
+		)
+
+		// Log a sanitized DSN (do not print password)
+		sanitized := fmt.Sprintf(
+			"host=%s user=%s dbname=%s port=%s sslmode=%s",
+			host, user, dbname, port, sslmode,
+		)
+		log.Printf("Using DSN: %s", sanitized)
 	}
+
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
